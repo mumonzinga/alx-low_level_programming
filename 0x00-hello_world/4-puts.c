f66b085742b4c2ac82a4cdf01617f789f667774d
mumonzinga <mnzinga888@gmail.com>
2023-06-21 12:55:46 -0700

    line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 /**
  * main - entry point
  * puts - c function automatically prints a new line'
- * return -0 
- */
+ * return -0. */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

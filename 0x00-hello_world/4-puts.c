85d4d4d5e4c71e6a4783fa8604711b41e54793cf
mumonzinga <mnzinga888@gmail.com>
2023-06-21 12:36:29 -0700

    line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 			{
-			puts("\"Programming is like building a multilingual puzzle"\n");
+			puts("Programming is like building a multilingual puzzle"\n");
 			returns(0);
 			}

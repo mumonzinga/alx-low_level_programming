5080d70d237e07e1e3f85f42a04eb3fff9a72441
mumonzinga <mnzinga888@gmail.com>
2023-07-02 11:26:06 -0700

    betty guides

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - entry point
+ *
  * puts - c function automatically prints a new line'
- * return -0. */
+ *
+ * return always 0 (success)
+ */
+
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }

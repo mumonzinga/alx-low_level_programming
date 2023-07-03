13aeafe1d948ee4f6e091e3c7438fa459e99b7d8
mumonzinga <mnzinga888@gmail.com>
2023-07-03 03:05:33 -0700

    print baase 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+		if (digit == '9')
+		{
+			digit = 'a';
+			for (; digit <= 'f'; digit++)
+			{
+				putchar(digit);
+			}
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

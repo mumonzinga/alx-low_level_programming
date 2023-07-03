821892aaefb210f31d3cc17726373d5ca109a717
mumonzinga <mnzinga888@gmail.com>
2023-07-03 12:06:27 -0700

    fill memory with const. byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -17,16 +17,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * declare an unsigned int
-	 * because we are storing a
-	 * value that will always be
-	 * non-negative (zero or positive)
-	*/
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
 	return (s);
 }

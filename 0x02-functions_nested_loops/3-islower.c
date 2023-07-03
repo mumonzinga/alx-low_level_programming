a23c5c48dbadcab8e5b4b9a08806273bf6f7d662
mumonzinga <mnzinga888@gmail.com>
2023-07-03 14:46:19 -0700

    alphas in lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * _islower - prints 1 or 0 depending on input
  *
  * Description: prints all lowercase letters
- *		#c: checks input of function
+ *		#int c: checks input of function
  *
  * Return: returns 1 if `c` is lowercase
  *         otherwise always 0 (Success)

40d6b10851a444f565d0ea3f942f3c051feedb6a
mumonzinga <mnzinga888@gmail.com>
2023-07-04 05:34:56 -0700

    print 0-9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdio.h>
+
 /**
 *main - header file for protoypes
 *void protoypes (void)
 * int protoypes (int)
 */
 
+void print_numbers(void);
 int _isupper(int);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
 
 #endif /* MAIN_H */
